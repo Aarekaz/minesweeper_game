5da8278fdc8a66b2106f232f9fc06a99b2f8fbad
Anurag Dhungana <36888347+Aarekaz@users.noreply.github.com>
2022-11-05 22:15:18 +0545

    initializing frames

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from tkinter import *
 
 root = Tk()
+#overide the setting of the video
+
+root.configure(background='black')
+root.geometry("1440x720")
+root.title("MINE SWEEPER")
+root.resizable(False,False)
+
+#creating frames
+
+top_frame= Frame(root,
+                 bg="white",
+                 width=1440,
+                 height=180
+                 )
+top_frame.place(x=0,y=0)
 
 
 
-
+#run the window
 root.mainloop()
